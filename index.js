0cfd6e2cf5828e65448ca8379ae290e15415cbe0
Lenna <pampam32121@gmail.com>
2022-07-13 04:44:19 +0300

    fetched data, displayed and styled it

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 'use strict';
 
+async function getUser() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/3');
+    const user = await res.json();
+    return user;
+}
+
+getUser().then(user => {
+    console.log(user);
+    document.body.innerHTML = `
+        <div class="user-profile">
+            <div class="user-profile-header">
+                <div>${user.name}</div>
+                <div>@${user.username}</div>
+            </div>
+            <div class="user-profile-company">
+                <div>👩🏽‍💼${user.company.name}</div>
+                <div>${user.company.catchphrase}</div>
+                <div>${user.company.bs}</div>
+            </div>
+            <div class="user-profile-contact">
+                <div>📧${user.email}</div>
+                <div>📞${user.phone}</div>
+                <div>💻${user.website}</div>
+            </div>
+            <div class="user-profile-address">
+                <div>${user.address.street}, ${user.address.suite}</div>
+                <div>${user.address.city} ${user.address.zipcode}</div>
+            </div>
+        </div>
+    `;
+
+});
